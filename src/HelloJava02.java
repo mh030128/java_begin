f5cc735dca945c9a21daee9319301ce1695305bc
sujin kim <mh030128@naver.com>
2024-11-11 17:07:01 +0900

    JavaProgram Start

---
+++ src/HelloJava02.java
@@ -0,0 +1,7 @@
+public class HelloJava02 {
+    public static void main(String[] args) {
+        System.out.println("hello java01");
+        System.out.println("hello java02");
+        System.out.println("hello java03");
+    }
+}
