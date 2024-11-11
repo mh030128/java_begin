d552d6a3a37d31d7303d2a78572fc2c302e31a45
sujin kim <mh030128@naver.com>
2024-11-11 17:12:53 +0900

    CommentTest

---
+++ src/CommentJava.java
@@ -0,0 +1,12 @@
+public class CommentJava {
+    public static void main(String[] args) {
+
+        System.out.println("hello java01"); // hello java01을 출력.
+        // System.out.println("hello java02");  // 한 줄 주석 - 라인 전체 적용
+
+        /* 여러 줄 주석
+        System.out.println("hello java03");
+        System.out.println("hello java04");
+         */
+    }
+}
