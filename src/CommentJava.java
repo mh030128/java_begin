2d594ee9117281338cad83eb5a87a19493ed280a
sujin kim <mh030128@naver.com>
2024-11-12 17:51:23 +0900

    CommentEx

--- src/CommentJava.java
+++ src/CommentJava.java
@@ -8,5 +8,16 @@ public class CommentJava {
         System.out.println("hello java03");
         System.out.println("hello java04");
          */
+
+        /*
+        * 주석(Comment)
+        * 소스코드 복잡 시 소스코드에 대한 이해 돕기 위해 설명 작성.
+        * 특정코드 지우지 않고 잠시 실행 막아두고 싶을 때 등 사용.
+        * 자바는 주석 있는 곳을 무시.
+        *
+        * 종류
+        * 1) 한 줄 주석(Single line) : //
+        * 2) 여러 줄 주석(Multi line) : /* */
+
     }
 }
