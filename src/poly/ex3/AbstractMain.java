b8ccb279f0a7f69507d8d544efcd4c69bcf91553
sujin kim <mh030128@naver.com>
2025-03-17 17:41:24 +0900

    polymorphismEx3

---
+++ src/poly/ex3/AbstractMain.java
@@ -0,0 +1,29 @@
+package poly.ex3;
+
+public class AbstractMain {
+    public static void main(String[] args) {
+
+        // 추상 클래스 생성 불가
+        // AbstractAnimal animal = new AbstractAnimal();
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Cow cow = new Cow();
+
+        cat.sound();
+        cat.move();
+
+        System.out.println();
+
+        soundAnimal(dog);
+        soundAnimal(cat);
+        soundAnimal(cow);
+    }
+
+    // 변하지 않는 부분
+    private static void soundAnimal(AbstractAnimal animal) {    // ctrl + alt + M
+        System.out.println("동물 소리 시작");
+        animal.sound();
+        System.out.println("동물 소리 종료");
+    }
+}
