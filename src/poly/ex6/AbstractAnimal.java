76d1a6a130e48a425c8bfa116ac12ee14f96594b
sujin kim <mh030128@naver.com>
2025-03-19 15:41:03 +0900

    polymorphismEx6

---
+++ src/poly/ex6/AbstractAnimal.java
@@ -0,0 +1,10 @@
+package poly.ex6;
+
+// 추상클래스
+public abstract class AbstractAnimal {
+
+    public abstract  void sound();  // 추상
+    public void move() {    // 상속목적
+        System.out.println("동물이 이동합니다.");
+    }
+}
